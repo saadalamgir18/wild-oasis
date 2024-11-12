c95f386af4c2eee8a9c8723e19dff3ffcf77e0ae
saadalamgir18 <saadalamgir18@gmail.com>
2024-11-13 00:16:59 +0500

    initialized wild oasis project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+export default function App() {
+  return (
+    <div>
+      <H1>hello world</H1>
+    </div>
+  );
+}
